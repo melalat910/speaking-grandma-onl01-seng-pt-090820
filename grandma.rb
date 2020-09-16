dd9c28bbbc863f6f53993ba257a30575f8618451
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-16 04:54:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,9 +8,9 @@ elsif phrase == "Hi Nana, how are you?"
   "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "Hi!"
   "HUH?! SPEAK UP SONNY!"
-else phrase ==  "WHAT DID YOU EAT TODAY?" 
+else phrase ==  "WHAT DID YOU EAT TODAY?"
   "NO, NOT SINCE 1938!"
-else phrase ==  "WHAT?" 
+else phrase ==  "WHAT?"
   "NO, NOT SINCE 1938!"
   end
  end
