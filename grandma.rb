2d97fe23d7d5dce414a759b53045f9b0ad780ad9
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-16 04:26:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method
 def speak_to_grandma(phrase)
 if phrase != phrase.upcase
-puts "HUH?! SPEAK UP SONNY!"
+ "HUH?! SPEAK UP SONNY!"
 elseif phrase == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
 else phrase ==  "WHAT DID YOU EAT TODAY?"
